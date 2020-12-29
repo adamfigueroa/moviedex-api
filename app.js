b1162864226a8515b9ca034ded213aa1f7cfe157
Adam Figueroa <admiralfiggins@gmail.com>
2020-12-29 14:26:15 -0800

    Setup first api test

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const morgan = require('morgan');
 const helmet = require('helmet')
 const cors = require('cors');
 
+const movieList = require("./movieList");
+
 const app = express();
 app.use(morgan('dev'));
 app.use(helmet())
 app.use(cors());
+
+app.get('/movie', (req, res) => {
+    const movies = [...movieList]
+    let { genre } = req.query
+
+    res.send('Yay I work')
+
+})
 
 app.listen(8000, () => {
     console.log('Express server is listening on port 8000!');
