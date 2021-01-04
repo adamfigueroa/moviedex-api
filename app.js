654336ff9505f30e9b86f1ba5af89def64cdca73
Adam Figueroa <admiralfiggins@gmail.com>
2021-01-04 09:05:02 -0800

    added error status for genre

--- app.js
+++ app.js
@@ -7,39 +7,67 @@ const cors = require("cors");
 const movieList = require("./movieList");
 
 const app = express();
+
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 
 app.get("/movie", (req, res) => {
-  const movies = [...movieList];
+  let movies = [...movieList];
+  let unfilteredGenreTypes = movies.map(movie => {
+    return movie.genre.toLowerCase()
+  })
+  let filteredGenreTypes = unfilteredGenreTypes.filter((c, index) => {
+    return unfilteredGenreTypes.indexOf(c) === index;
+  });
+  console.log(filteredGenreTypes)
+  
   let { genre, country } = req.query;
-  let genresFilter = [];
-  let countryFilter = [];
-
+  
   const capitalize = (s) => {
     if (typeof s !== "string") return "";
     return s.charAt(0).toUpperCase() + s.slice(1);
   };
 
-  if (genre) {
-    genresFilter = movies.filter((movie) =>
+  if (genre && country) {
+    let movieGenre = movies.filter((movie) =>
       movie.genre.toLowerCase().includes(genre.toLowerCase())
     );
-  };
-
-  if (genre === undefined) {
-    genresFilter = movies
-};
-
-if (country) {
-    countryFilter = movies.filter((movie) =>
+    let movieCountry = movieGenre.filter((movie) =>
       movie.country.toLowerCase().includes(country.toLowerCase())
     );
-    return res.json(countryFilter)
+    if (movieCountry.length === 0) {
+      return res
+        .status(400)
+        .json({
+          error: `Sorry, there are no matches for your chosen genre: ${genre} and country: ${country}`,
+        });
+    } else return res.json(movieCountry);
   };
 
-  res.json(genresFilter);
+  if (genre && !filteredGenreTypes.includes(genre.toLowerCase())) {
+    return res
+    .status(400)
+    .json({
+      error: `Sorry, you need to choose one of the following genres: ${filteredGenreTypes}`,
+    });
+  }
+
+  if (genre) {
+    movies = movies.filter((movie) =>
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+    return res.json(movies);
+  };
+
+  if (country) {
+    movies = movies.filter((movie) =>
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    );
+    return res.json(movies);
+  };
+
+  res.json(movies);
 });
 
 app.listen(8000, () => {
