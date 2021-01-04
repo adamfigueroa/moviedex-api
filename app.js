1dc12245e13a97aaddf9add771553332bdeb8747
Adam Figueroa <admiralfiggins@gmail.com>
2021-01-04 09:26:38 -0800

    added avg_vote option and token authentication

--- app.js
+++ app.js
@@ -12,22 +12,28 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 
+app.use(
+  (validateBearerToken = (req, res, next) => {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get("Authorization");
+    if (!authToken || authToken.split(" ")[1] !== apiToken) {
+      return res.status(401).json({ error: "Unauthorized access" });
+    }
+    next();
+  })
+);
+
 app.get("/movie", (req, res) => {
   let movies = [...movieList];
-  let unfilteredGenreTypes = movies.map(movie => {
-    return movie.genre.toLowerCase()
-  })
+  let unfilteredGenreTypes = movies.map((movie) => {
+    return movie.genre.toLowerCase();
+  });
   let filteredGenreTypes = unfilteredGenreTypes.filter((c, index) => {
     return unfilteredGenreTypes.indexOf(c) === index;
   });
-  console.log(filteredGenreTypes)
-  
-  let { genre, country } = req.query;
-  
-  const capitalize = (s) => {
-    if (typeof s !== "string") return "";
-    return s.charAt(0).toUpperCase() + s.slice(1);
-  };
+  console.log(filteredGenreTypes);
+
+  let { genre, country, avg_vote } = req.query;
 
   if (genre && country) {
     let movieGenre = movies.filter((movie) =>
@@ -37,18 +43,14 @@ app.get("/movie", (req, res) => {
       movie.country.toLowerCase().includes(country.toLowerCase())
     );
     if (movieCountry.length === 0) {
-      return res
-        .status(400)
-        .json({
-          error: `Sorry, there are no matches for your chosen genre: ${genre} and country: ${country}`,
-        });
+      return res.status(400).json({
+        error: `Sorry, there are no matches for your chosen genre: ${genre} and country: ${country}`,
+      });
     } else return res.json(movieCountry);
-  };
+  }
 
   if (genre && !filteredGenreTypes.includes(genre.toLowerCase())) {
-    return res
-    .status(400)
-    .json({
+    return res.status(400).json({
       error: `Sorry, you need to choose one of the following genres: ${filteredGenreTypes}`,
     });
   }
@@ -58,14 +60,20 @@ app.get("/movie", (req, res) => {
       movie.genre.toLowerCase().includes(genre.toLowerCase())
     );
     return res.json(movies);
-  };
+  }
 
   if (country) {
     movies = movies.filter((movie) =>
       movie.country.toLowerCase().includes(country.toLowerCase())
     );
     return res.json(movies);
-  };
+  }
+
+  if (avg_vote) {
+    movies = movies.filter(
+      (movie) => Number(movie.avg_vote) >= Number(avg_vote)
+    );
+  }
 
   res.json(movies);
 });
